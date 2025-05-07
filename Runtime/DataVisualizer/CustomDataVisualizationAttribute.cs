f106652a843572af9d802eeb987a9c741641db11
Eli Pinkerton <wallstop@wallstopstudios.com>
2025-05-06 19:26:30 -0700

    Minor enhancements (#2)
    * Add custom type name
    
    * Update version

--- Runtime/DataVisualizer/CustomDataVisualizationAttribute.cs
+++ Runtime/DataVisualizer/CustomDataVisualizationAttribute.cs
@@ -3,9 +3,11 @@
     using System;
 
     [AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = true)]
-    public sealed class CustomDataVisualization : Attribute
+    public sealed class CustomDataVisualizationAttribute : Attribute
     {
         public string Namespace { get; set; }
+
+        public string TypeName { get; set; }
 #if ODIN_INSPECTOR
         public bool UseOdinInspector { get; set; } = true;
 #endif
