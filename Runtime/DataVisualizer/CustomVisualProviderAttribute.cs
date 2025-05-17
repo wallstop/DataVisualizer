8ef77e29cf88b1e3f6e542355497fa3770fadde6
Eli Pinkerton <wallstop@wallstopstudios.com>
2025-05-16 20:14:07 -0700

    Remove no-longer-needed CustomVisualProvider Attribute

--- Runtime/DataVisualizer/CustomVisualProviderAttribute.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace WallstopStudios.DataVisualizer
-{
-    using System;
-
-    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false, Inherited = true)]
-    public sealed class CustomVisualProviderAttribute : Attribute { }
-}
