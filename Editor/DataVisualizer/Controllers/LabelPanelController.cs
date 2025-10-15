87f003148319765e0312101782afba18a581e299
wallstop <wallstop@wallstopstudios.com>
2025-10-15 10:29:19 -0700

    Compilation fixes

--- Editor/DataVisualizer/Controllers/LabelPanelController.cs
+++ Editor/DataVisualizer/Controllers/LabelPanelController.cs
@@ -332,7 +332,7 @@ namespace WallstopStudios.DataVisualizer.Editor.Controllers
             {
                 style =
                 {
-                    color = _dataVisualizer.IsColorDark(labelColor) ? Color.white : Color.black,
+                    color = DataVisualizer.IsColorDark(labelColor) ? Color.white : Color.black,
                     marginRight =
                         currentSection == DataVisualizer.LabelFilterSection.AND
                         || currentSection == DataVisualizer.LabelFilterSection.OR
