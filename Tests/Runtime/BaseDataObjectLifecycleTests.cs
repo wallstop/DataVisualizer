f210599606e3068f640b9f2d46dba07809c7f3c4
wallstop <wallstop@wallstopstudios.com>
2026-09-09 15:30:53 +0000

    Add runtime lifecycle PlayMode coverage

---
+++ Tests/Runtime/BaseDataObjectLifecycleTests.cs
@@ -0,0 +1,72 @@
+namespace WallstopStudios.DataVisualizer.Tests.Runtime
+{
+    using NUnit.Framework;
+    using UnityEngine;
+    using WallstopStudios.DataVisualizer;
+
+    public sealed class BaseDataObjectLifecycleTests
+    {
+        private TestDataObject clone;
+        private TestDataObject source;
+
+        [SetUp]
+        public void SetUp()
+        {
+            source = ScriptableObject.CreateInstance<TestDataObject>();
+            clone = ScriptableObject.CreateInstance<TestDataObject>();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Object.DestroyImmediate(source);
+            Object.DestroyImmediate(clone);
+        }
+
+        [Test]
+        public void Should_ExposeLifecycleContracts_WhenCreated()
+        {
+            Assert.That(source, Is.InstanceOf<ICreatable>());
+            Assert.That(source, Is.InstanceOf<IDuplicable>());
+            Assert.That(source, Is.InstanceOf<IRenamable>());
+            Assert.That(source, Is.InstanceOf<IGUIProvider>());
+            Assert.That(source, Is.InstanceOf<IDisplayable>());
+        }
+
+        [Test]
+        public void Should_ClearIdentityAndIncrementTitle_WhenCloneHooksRun()
+        {
+            source.Title = "Sword";
+            clone.SetAssetGuid("stale-guid");
+
+            clone.BeforeClone(source);
+            clone.AfterClone(source);
+
+            Assert.That(clone.Id, Is.Empty);
+            Assert.That(clone.Title, Is.EqualTo("Sword (Clone)"));
+        }
+
+        [TestCase("Sword (Clone)", "Sword (Clone 1)")]
+        [TestCase("Sword (Clone 4)", "Sword (Clone 5)")]
+        public void Should_ContinueCloneNumbering_WhenSourceAlreadyHasCloneSuffix(
+            string sourceTitle,
+            string expectedCloneTitle
+        )
+        {
+            source.Title = sourceTitle;
+
+            clone.BeforeClone(source);
+            clone.AfterClone(source);
+
+            Assert.That(clone.Title, Is.EqualTo(expectedCloneTitle));
+        }
+
+        private sealed class TestDataObject : BaseDataObject
+        {
+            public void SetAssetGuid(string value)
+            {
+                _assetGuid = value;
+            }
+        }
+    }
+}
